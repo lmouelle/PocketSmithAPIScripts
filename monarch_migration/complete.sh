893c4046797a10db3efcfbbb7bd4dfd2bbfb3988
Lucas Falkenstein <lmouelle@users.noreply.github.com>
2025-03-30 21:04:35 -0400

    trim old gunk

--- monarch_migration/complete.sh
+++ monarch_migration/complete.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/bash
 
-ps_file=/home/luouelle/Nextcloud/Archive/finance_app/pocketsmith-transactions-2024-12-22.csv
-mon_file=/var/home/luouelle/Nextcloud/Archive/finance_app/monarch-money-transactions-2024-12-29.csv
-tmp_name=tmp.txt
-
-./detect_ps_dups.py ~/Nextcloud/Archive/finance_app/pocketsmith-transactions-2024-12-22.csv \
-| grep -v SEPTA \
-| grep -v TGTG \
-| grep -v "MTA\*NYCT" \
-| grep -v "Transfer & Card Payments"
+./importer.py --discover ~/Nextcloud/Archive/finance_app/Discover_Bank_Checking.csv \
+              --discover ~/Nextcloud/Archive/finance_app/Discover_Bank_Checking.
