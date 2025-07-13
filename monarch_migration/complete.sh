808e29b4d2cc83242e43b7cb9c16467ed977cbc9
Lucas Falkenstein <lmouelle@users.noreply.github.com>
2025-07-13 17:28:40 -0400

    trim dead files

--- monarch_migration/complete.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-./importer.py --discover ~/Nextcloud/Archive/finance_app/Discover_Bank_Checking.csv \
-              --discover ~/Nextcloud/Archive/finance_app/Discover_Bank_Checking.
